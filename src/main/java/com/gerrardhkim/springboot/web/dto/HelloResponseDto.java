c2c12abc482ee65f34442ad9c10c2c17be0a7be3
Gerrard <cjsgk106@gmail.com>
2020-10-28 03:09:45 +0900

    JPA implemented.

---
+++ src/main/java/com/gerrardhkim/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.gerrardhkim.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
